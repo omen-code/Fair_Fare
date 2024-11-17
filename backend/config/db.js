11bbb9da2711528a6a34eeb1dc8f6e1c59228532
omen-code <nishhegde123@gmail.com>
2024-11-17 14:52:36 +0530

    allgone

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectdb=async()=>{await mongoose.connect('mongodb+srv://nishanthegde2005:9480306549@cluster0.6cvha.mongodb.net/?retryWrites=true&w=majority').then(()=>console.log("DB connected"));
-
-}
